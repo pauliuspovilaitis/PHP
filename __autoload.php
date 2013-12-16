bcff69fde9d1b228c7098ccef9d753a6288f1a01
Paulius Povilaitis <paulius.povilaitis@gmail.com>
2013-12-16 16:22:18 +0200

    Create __autoload.php

---
+++ __autoload.php
@@ -0,0 +1,15 @@
+function __autoload($class_name) {
+	if (file_exists($class_name . '.php'))
+	{		
+		require_once "$class_name.php";
+	}
+	else 
+	{
+		print "autoload can not load the $class_name"; 
+		print " exiting...";
+		exit;
+	}	
+}
+
+$obj  = new MyClass1();
+$obj2 = new MyClass2();
