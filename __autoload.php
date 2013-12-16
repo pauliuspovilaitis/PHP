acf0012d489e9cd7878dc0196c8e6de49719ad3d
Paulius Povilaitis <paulius.povilaitis@gmail.com>
2013-12-16 16:25:30 +0200

    Update __autoload.php

--- __autoload.php
+++ __autoload.php
@@ -1,12 +1,12 @@
 function __autoload($class_name) {
 	if (file_exists($class_name . '.php'))
 	{		
-		require_once $class_name.'.php';
+		require_once $class_name . '.php';
 	}
 	else 
 	{
-		print "autoload can not load the $class_name"; 
-		print " exiting...";
+		print 'autoload can not load the ' . $class_name; 
+		print ' exiting...';
 		exit;
 	}	
 }
