d38eb91c9c95aface554cbf40f965e7e6f416067
Paulius Povilaitis <paulius.povilaitis@gmail.com>
2013-12-29 10:55:26 +0200

    Update security.php

--- security.php
+++ security.php
@@ -1,6 +1,16 @@
-//security:
 
 $a = "C:/dir/file.php";
 echo basename($a);
+//outpu: file.php
+//escapes dir to avoid injection. 
 
-//file.php
+htmlspecialchars ($string);  
+// convert &,”,’,<,> into HTML entities e.g. &quot
+
+
+htmlentities ($string,FLAGS);  
+// converts all applicable to HTML entities 
+// (European accents, etc.), only necessary if your pages use encodings such as ASCII or LATIN-1 instead of UTF-8.
+
+strip_tags ($string,$allowable_tags);   
+// remove all tags [opening&closing] (except $allowable_tags) from the string
